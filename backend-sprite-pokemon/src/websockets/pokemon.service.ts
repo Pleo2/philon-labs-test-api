ae840d15ca359ce89ef70b398e13f3e66b6041b7
p_leo2 <leooel23m@gmail.com>
2024-04-10 12:04:19 -0400

    add: axios and Pokemon service

--- backend-sprite-pokemon/src/websockets/pokemon.service.ts
+++ backend-sprite-pokemon/src/websockets/pokemon.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { UrlObject } from 'url';
+import axios from 'axios';
 
 @Injectable()
 export class PokemonService {
+  async getSprite(id: number): Promise<any> {
+    const data = await fetch(
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+    );
+    return data.url;
+  }
+
+  async downloadImage(url: string) {
+    const response = await axios.get(url, {
+      responseType: 'arraybuffer',
+    });
+    const imageBuffer = Buffer.from(response.data, 'binary');
+    return imageBuffer;
+  }
 }
