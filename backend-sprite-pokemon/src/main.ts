46e387101713e9043bae3112c8d76d7844ba1281
p_leo2 <leooel23m@gmail.com>
2024-04-09 20:53:28 -0400

    add backend

---
+++ backend-sprite-pokemon/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT ?? 3001;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+  });
+}
+bootstrap();
